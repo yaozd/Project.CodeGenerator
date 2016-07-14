8f1df3ea4b9cb771ac732f5480be860397364ce0
zd.yao <2674290996@qq.com>
2016-07-14 10:29:12 +0800

    update -daoauto

--- Project.CodeGenerator/Utils/RegexPub.cs
+++ Project.CodeGenerator/Utils/RegexPub.cs
@@ -6,11 +6,11 @@ namespace Project.CodeGenerator.Utils
     {
         public static Regex H1()
         {
-            return new Regex(@"<h1>|</h1>", RegexOptions.Compiled);
+            return new Regex(@"<h1>[\s]*|</h1>[\s]*", RegexOptions.Compiled);
         }
         public static Regex H2()
         {
-            return new Regex(@"<h2>|</h2>", RegexOptions.Compiled);
+            return new Regex(@"<h2>[\s]*|</h2>[\s]*", RegexOptions.Compiled);
         }
     }
 }
